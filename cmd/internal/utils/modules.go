5be6b07beb5ceffd48ff0acb9b733898bbc7d38f
PYRS <yagosousa2512@gmail.com>
2025-08-23 20:34:06 -0300

    Update: Novas funções

---
+++ cmd/internal/utils/modules.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+func AsyncWithLimit(maxConcurrent int, wg *sync.WaitGroup, f func()) {
+	sem := make(chan struct{}, maxConcurrent)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		sem <- struct{}{}        // ocupa uma "vaga"
+		defer func() { <-sem }() // libera a "vaga"
+
+		f()
+	}()
+}
+
+func Async(wg *sync.WaitGroup, f func()) {
+	AsyncWithLimit(-1, wg, f)
+}
+
+func ExecWithAttempts(maxAtt int, f func() error) error {
+	var err error
+
+	for att := 1; att <= maxAtt; att++ {
+		if err = f(); err == nil {
+			return nil
+		}
+		log.Printf("ERROR | Attempt %d/%d | %v", att, maxAtt, err)
+	}
+
+	return fmt.Errorf("max attempts reached | last error: %w", err)
+}
