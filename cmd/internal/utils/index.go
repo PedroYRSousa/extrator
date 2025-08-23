15f4a344cfbff23682921cb31b6378153c22e6f3
PYRS <yagosousa2512@gmail.com>
2025-08-23 19:57:20 -0300

    Update: Separando verificadores

--- cmd/internal/utils/index.go
+++ cmd/internal/utils/index.go
@@ -6,26 +6,38 @@ import (
 	"regexp"
 )
 
-func CheckIsHardCodedSecretOrEnv(target string) bool {
-	var baseRegex = `[0-9a-zA-Z-_\.]*`
+const baseRegex = `[0-9a-zA-Z-_\.]*`
 
+func IsSecret(target string) bool {
+	secretRegex, err := regexp.Compile(fmt.Sprintf(`^secret\(%s\)$`, baseRegex))
+	if err != nil {
+		log.Fatalf("failed to compile regex for secret and env validation: %v", err)
+		return false
+	}
+
+	return secretRegex.Match([]byte(target))
+}
+
+func IsEnv(target string) bool {
+	envRegex, err := regexp.Compile(fmt.Sprintf(`^env\(%s\)$`, baseRegex))
+	if err != nil {
+		log.Fatalf("failed to compile regex for secret and env validation: %v", err)
+		return false
+	}
+
+	return envRegex.Match([]byte(target))
+}
+
+func IsHardcoded(target string) bool {
 	hardCodedRegex, err := regexp.Compile(baseRegex)
 	if err != nil {
 		log.Fatalf("failed to compile regex for secret and env validation: %v", err)
 		return false
 	}
 
-	secretRegex, err := regexp.Compile(fmt.Sprintf(`secret\(%s\)`, baseRegex))
-	if err != nil {
-		log.Fatalf("failed to compile regex for secret and env validation: %v", err)
-		return false
-	}
+	return hardCodedRegex.Match([]byte(target))
+}
 
-	envRegex, err := regexp.Compile(fmt.Sprintf(`env\(%s\)`, baseRegex))
-	if err != nil {
-		log.Fatalf("failed to compile regex for secret and env validation: %v", err)
-		return false
-	}
-
-	return hardCodedRegex.Match([]byte(target)) || secretRegex.Match([]byte(target)) || envRegex.Match([]byte(target))
+func CheckIsHardCodedSecretOrEnv(target string) bool {
+	return IsHardcoded(target) || IsEnv(target) || IsSecret(target)
 }
