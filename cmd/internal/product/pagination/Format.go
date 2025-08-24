d1d0ecb23419aedc09756f45f7c7e2a869decb01
PYRS <yagosousa2512@gmail.com>
2025-08-24 20:34:15 -0300

    Update

--- cmd/internal/product/pagination/Format.go
+++ cmd/internal/product/pagination/Format.go
@@ -3,42 +3,45 @@ package pagination
 import "strings"
 
 func (po *S_PaginationOffset) format() {
-	if po != nil {
-		po.Direction = paginationDirection(strings.TrimSpace(strings.ToLower(string(po.Direction))))
-		po.Location = paginationLocation(strings.TrimSpace(strings.ToLower(string(po.Location))))
-		po.Offset = strings.TrimSpace(po.Offset)
-		po.Limit = strings.TrimSpace(po.Limit)
+	if po == nil {
+		panic("Offset is required")
 	}
+
+	po.Direction = paginationDirection(strings.TrimSpace(strings.ToLower(string(po.Direction))))
+	po.Location = paginationLocation(strings.TrimSpace(strings.ToLower(string(po.Location))))
+	po.Offset = strings.TrimSpace(po.Offset)
+	po.Limit = strings.TrimSpace(po.Limit)
 }
 
 func (pp *S_PaginationPage) format() {
-	if pp != nil {
-		pp.Direction = paginationDirection(strings.TrimSpace(strings.ToLower(string(pp.Direction))))
-		pp.Location = paginationLocation(strings.TrimSpace(strings.ToLower(string(pp.Location))))
-		pp.Page = strings.TrimSpace(pp.Page)
-		if pp.PageSize <= 0 {
-			pp.PageSize = 1
-		}
+	if pp == nil {
+		panic("Page is required")
+	}
+
+	pp.Direction = paginationDirection(strings.TrimSpace(strings.ToLower(string(pp.Direction))))
+	pp.Location = paginationLocation(strings.TrimSpace(strings.ToLower(string(pp.Location))))
+	pp.Page = strings.TrimSpace(pp.Page)
+	if pp.PageSize <= 0 {
+		pp.PageSize = 1
 	}
 }
 
 func (pp *S_PaginationProperty) format() {
-	if pp != nil {
-		pp.Property = strings.TrimSpace(pp.Property)
+	if pp == nil {
+		panic("Property is required")
 	}
+
+	pp.Property = strings.TrimSpace(pp.Property)
 }
 
 func (plh *S_PaginationLinkHeader) format() {
-	if plh != nil {
-		plh.Header = strings.TrimSpace(plh.Header)
+	if plh == nil {
+		panic("LinkHeader is required")
 	}
+
+	plh.Header = strings.TrimSpace(plh.Header)
 }
 
 func (p *S_Pagination) format() {
 	p.Mode = paginationMode(strings.TrimSpace(strings.ToLower(string(p.Mode))))
-
-	p.Offset.format()
-	p.Page.format()
-	p.Property.format()
-	p.LinkHeader.format()
 }
--- cmd/internal/product/pagination/Validate.go
+++ cmd/internal/product/pagination/Validate.go
@@ -7,80 +7,90 @@ import (
 )
 
 func (po *S_PaginationOffset) validate() error {
+	po.format()
+
 	if po.Direction == "" {
-		return errors.New("invalid pagination | Check pagination.offset.direction | value cannot be empty")
+		return errors.New("check pagination.offset.direction | value cannot be empty")
 	}
 
-	if !slices.Contains(availableDirections, po.Direction) {
-		return fmt.Errorf("invalid pagination | Check pagination.offset.direction | available options: %v", availableDirections)
+	if !slices.Contains(AVAILABLE_DIRECTIONS, po.Direction) {
+		return fmt.Errorf("check pagination.offset.direction | available options: %v", AVAILABLE_DIRECTIONS)
 	}
 
 	if po.Location == "" {
-		return errors.New("invalid pagination | Check pagination.offset.location | value cannot be empty")
+		return errors.New("check pagination.offset.location | value cannot be empty")
 	}
 
-	if !slices.Contains(availableLocations, po.Location) {
-		return fmt.Errorf("invalid pagination | Check pagination.offset.location | available options: %v", availableLocations)
+	if !slices.Contains(AVAILABLE_LOCATIONS, po.Location) {
+		return fmt.Errorf("check pagination.offset.location | available options: %v", AVAILABLE_LOCATIONS)
 	}
 
 	if po.Offset == "" {
-		return errors.New("invalid pagination | Check pagination.offset.offset | value cannot be empty")
+		return errors.New("check pagination.offset.offset | value cannot be empty")
 	}
 
 	if po.Limit == "" {
-		return errors.New("invalid pagination | Check pagination.offset.limit | value cannot be empty")
+		return errors.New("check pagination.offset.limit | value cannot be empty")
 	}
 
 	return nil
 }
 
 func (pp *S_PaginationPage) validate() error {
+	pp.format()
+
 	if pp.Direction == "" {
-		return errors.New("invalid pagination | Check pagination.page.direction | value cannot be empty")
+		return errors.New("check pagination.page.direction | value cannot be empty")
 	}
 
-	if !slices.Contains(availableDirections, pp.Direction) {
-		return fmt.Errorf("invalid pagination | Check pagination.page.direction | available options: %v", availableDirections)
+	if !slices.Contains(AVAILABLE_DIRECTIONS, pp.Direction) {
+		return fmt.Errorf("check pagination.page.direction | available options: %v", AVAILABLE_DIRECTIONS)
 	}
 
 	if pp.Location == "" {
-		return errors.New("invalid pagination | Check pagination.page.location | value cannot be empty")
+		return errors.New("check pagination.page.location | value cannot be empty")
 	}
 
-	if !slices.Contains(availableLocations, pp.Location) {
-		return fmt.Errorf("invalid pagination | Check pagination.page.location | available options: %v", availableLocations)
+	if !slices.Contains(AVAILABLE_LOCATIONS, pp.Location) {
+		return fmt.Errorf("check pagination.page.location | available options: %v", AVAILABLE_LOCATIONS)
 	}
 
 	if pp.Page == "" {
-		return errors.New("invalid pagination | Check pagination.page.page | value cannot be empty")
+		return errors.New("check pagination.page.page | value cannot be empty")
 	}
 
 	if pp.PageSize <= 0 {
-		return errors.New("invalid pagination | Check pagination.page.page_size | value must be greater than 0")
+		return errors.New("check pagination.page.page_size | value must be greater than 0")
 	}
 
 	return nil
 }
 
 func (pp *S_PaginationProperty) validate() error {
+	pp.format()
+
 	if pp.Property == "" {
-		return errors.New("invalid pagination | Check pagination.property.property | value cannot be empty")
+		return errors.New("check pagination.property.property | value cannot be empty")
 	}
 
 	return nil
 }
 
 func (plh *S_PaginationLinkHeader) validate() error {
+	plh.format()
+
 	if plh.Header == "" {
-		return errors.New("invalid pagination | Check pagination.link_header.header | value cannot be empty")
+		return errors.New("check pagination.link_header.header | value cannot be empty")
 	}
 
 	return nil
 }
 
 func (p *S_Pagination) Validate() error {
-	if !slices.Contains(availableModes, p.Mode) {
-		return fmt.Errorf("invalid pagination | Check pagination.mode | available options: %v", availableModes)
+	p.format()
+
+	if !slices.Contains(AVAILABLE_MODES, p.Mode) {
+		return fmt.Errorf("check pagination.mode | available options: %v", AVAILABLE_MODES)
 	}
 
 	if p.Mode == PAGINATION_MODE_NONE {
@@ -88,22 +98,26 @@ func (p *S_Pagination) Validate() error {
 	}
 
 	switch p.Mode {
-	case PAGINATION_MODE_LINK_HEADER:
-		if err := p.LinkHeader.validate(); err != nil {
-			return err
-		}
-	case PAGINATION_MODE_PROPERTY:
-		if err := p.Property.validate(); err != nil {
-			return err
-		}
-	case PAGINATION_MODE_PAGE:
-		if err := p.Page.validate(); err != nil {
-			return err
-		}
 	case PAGINATION_MODE_OFFSET:
-		if err := p.Offset.validate(); err != nil {
-			return err
+		if p.Offset == nil {
+			return errors.New("check pagination.offset | offset cannot be null when mode is 'offset'")
 		}
+		return p.Offset.validate()
+	case PAGINATION_MODE_PAGE:
+		if p.Page == nil {
+			return errors.New("check pagination.page | page cannot be null when mode is 'page'")
+		}
+		return p.Page.validate()
+	case PAGINATION_MODE_PROPERTY:
+		if p.Property == nil {
+			return errors.New("check pagination.property | property cannot be null when mode is 'property'")
+		}
+		return p.Property.validate()
+	case PAGINATION_MODE_LINK_HEADER:
+		if p.LinkHeader == nil {
+			return errors.New("check pagination.link_header | link_header cannot be null when mode is 'link_header'")
+		}
+		return p.LinkHeader.validate()
 	}
 
 	return nil
